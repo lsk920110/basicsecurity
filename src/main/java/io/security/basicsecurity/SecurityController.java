708e1bc7202f1f035775d3d44a56a6d9de7cf6a1
lsk <lsk@rowem.com>
2022-11-02 18:07:19 +0900

    Initial commit

---
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -0,0 +1,16 @@
+package io.security.basicsecurity;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String main(){
+
+        return "home";
+    }
+
+}
